795019b096e3f28d49723f7a12f54124a8da2154
Yuri BigEye <occhio.yuri@gmail.com>
2026-03-02 18:08:53 +0100

    Rifinitura post correzione in classe

--- esercizi_lezione_2.py
+++ esercizi_lezione_2.py
@@ -1,12 +1,16 @@
 #print 538 minutes in hours format
 print("ESERCIZIO 1)\n")
 minutes = 538
-hours = minutes // 60
+hours = minutes // 60 #Divisione intera
+
+#hours = int(minutes/60) alternativa con casting
+
 remaining_minutes = minutes % 60
 print(f"Minutes: {minutes} = Hours: {hours}h {remaining_minutes}m")
 
 print("\n\n")
 
+#=======================================================================================================================
 #Ask to the user an integer and print its square and his cube
 print("ESERCIZIO 2)\n")
 print("Scrivi un numero intero: ")
@@ -15,8 +19,19 @@ print(f"Il numero inserito e': {user_input}")
 print(f"Il quadrato e': {user_input ** 2}")
 print(f"Il cubo e' {user_input ** 3}")
 
+#Alternativa
+def quadrato(number):
+    quadrato = number ** 2
+    return quadrato
+
+def cubo(number):
+    cubo = number ** 3
+    return cubo
+
+
 print("\n\n")
 
+#========================================================================================================================
 #Verify if an inserted number is even or odd
 print("ESERCIZIO 3)\n")
 print("Inserisci un numero intero: ")
@@ -29,43 +44,42 @@ else:
 
 print("\n\n")
 
+#===========================================================================================================================
 #Define a function that takes as argumnet a word and a character and return how many times the character appears in the word
 print("ESERCIZIO 4)\n")
 def count_character(word, char):
+    word = word.lower()
+    char = char.lower()
     count = 0
-    for chars in word:
-        if chars == char:
+    for characters in word:
+        if characters == char:
             count += 1
     return count
 
 print("Inserisci una parola: ")
-word = str(input())
-print("Inserisci una lettera: ")
-char = str(input())
-
+word = str(input()) #We can also use word = str(input("Inserisci una parola: "))
+char = str(input("Inserisci una lettera: "))
 print(count_character(word, char))
+
 print("\n\n")
 
-
+#=============================================================================================================================
 #Write a program that verify if a number inserted is prime
 print("ESERCIZIO 5)\n")
-print("Enter a number to check if is prime")
-user_input = int(input())
 
-def is_prime(number):
-    if number <= 3:
-        return True
-    elif number % 2 == 0:
-        return False
-    for i in range(3, number, 2):
-        if number % i == 0:
-            return False
-    return True
+def is_prime():
+    number = int(input("Enter a number to check if is prime: "))
+    for i in range(2, number):
+        if (number%i==0):
+            print(f"Il numero {number} non e' primo!")
+            return
+    print(f"Il numero {number} e' primo!")
 
-print(is_prime(user_input))
+is_prime()
 
 print("\n\n")
 
+#==============================================================================================================================
 #write a program that sum n numbers inserted by the user
 print("ESERCIZIO 6)\n")
 def sum_numbers():
@@ -82,6 +96,7 @@ sum_numbers()
 
 print("\n\n")
 
+#=================================================================================================================================
 #write a program that compute the factorial through an iterative function
 print("ESERCIZIO 7)\n")
 def factorial():
@@ -101,6 +116,7 @@ print(factorial())
 
 print("\n\n")
 
+#==================================================================================================================================
 #scrivi un programma che dice all'utente se 3 numeri inseriti possono essere i lati di un triangolo ed eventualmente di quale tipo
 print("ESERCIZIO 8)\n")
 def is_triangle():
