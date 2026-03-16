e04b88818ea04719cd3c670d63297d9f3d8a7943
Yuri BigEye <occhio.yuri@gmail.com>
2026-03-16 11:40:15 +0100

    Aggiunti approfondimenti su for loop

---
+++ esercizi_lez_3_approfondimento.py
@@ -0,0 +1,12 @@
+for idx in range(len(lista)):
+    print(idx) #<-- stampa il valore dell'indice
+
+for elem in lista:
+    print(elem) #<-- stampa ciascun elemento della lista
+
+for idx in lista:
+    print(idx, lista[idx])
+
+for idx, elem in enumerate(lista):
+    print(idx, elem)
+
